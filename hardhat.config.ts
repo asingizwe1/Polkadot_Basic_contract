347cf82c4c9d5def6ccd9b0bd9c76d27b90dce05
asingizwe1 <asingizwe1louis1@gmail.com>
2026-01-28 20:33:27 +0300

    initialisation of hardhart polkadot project

---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    polkadotTestnet: {
+      type: "http", // 👈 required in Hardhat 3.x
+      url: "https://services.polkadothub-rpc.com/testnet",
+      chainId: 420420417,
+      accounts: [process.env.POLKADOT_TESTNET_PRIVATE_KEY!],
+    },
+  },
+};
+
+export default config;
